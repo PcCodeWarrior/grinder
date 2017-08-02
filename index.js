6d8aef76989b40adea048b59ad6cfb4973c6db2b
pccodewarrior <pc_code_warrior@yahoo.com>
2017-08-02 10:28:35 -0600

    add mongo connection

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const  express        = require('express');
 const  app            = express();
+const  mongoose       = require('mongoose');
+const  keys           = require('./config/keys');
+
 
 //const passportConfig =require('./services/passport');  since this does not return anything it can also be written like:
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 //const authRoutes = require('./routes/authRoutes');  following 2 lines can also be written as:
 //authRoutes(app);
