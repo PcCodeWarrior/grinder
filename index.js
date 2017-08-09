34ba955fe07ca039b2e78d48c20f77a5db456dc0
pccodewarrior <pc_code_warrior@yahoo.com>
2017-08-09 11:22:52 -0600

    work on auth

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const  express        = require('express');
-const  app            = express();
+
 const  mongoose       = require('mongoose');
 const  keys           = require('./config/keys');
 
 
 //const passportConfig =require('./services/passport');  since this does not return anything it can also be written like:
-require('./services/passport');
+
 require('./models/User');
+require('./services/passport');
+
 
 mongoose.connect(keys.mongoURI);
+
+const  app            = express();
 
 //const authRoutes = require('./routes/authRoutes');  following 2 lines can also be written as:
 //authRoutes(app);
